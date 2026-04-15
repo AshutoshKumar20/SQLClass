faec392e076c9854511639cf7ed299ac328aa19e
Ashutosh Kumar <ashutoshkumar44549@gmail.com>
2026-04-15 13:06:58 +0530

    Connected with the sql server

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 const { faker } = require('@faker-js/faker');
+const mysql = require('mysql2');
 
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'gamma',
+    password: 'ashu@1234',
+});
+
+try {
+    connection.query("SHOW DATABASES", (err, results) => {
+        if (err) throw err;
+        console.log(results);
+
+    });
+} catch (err) {
+    console.log(err);
+};
+
+connection.end();
 let getRandomUser = () => {
     return {
         id: faker.string.uuid(),
@@ -8,5 +27,3 @@ let getRandomUser = () => {
         password: faker.internet.password(),
     };
 }
-
-console.log(getRandomUser());
